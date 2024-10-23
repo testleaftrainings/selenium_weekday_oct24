a15fc7dfdb886e5a83deaa4290eb18d05bd3dd3c
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-10-23 15:41:35 +0530

    Add files via upload

---
+++ week2/Day3/Auto.java
@@ -0,0 +1,17 @@
+package week2.day3;
+
+
+
+public class Auto extends Vehicle{
+	public void applyMeter() {
+		System.out.println("pay the amount--->Auto");
+		
+	}
+	public static void main(String[] args) {
+		
+		
+		Auto A=new Auto();
+		A.applyMeter();
+		
+	}
+}
