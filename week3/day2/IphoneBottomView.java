985e40093b1bf6d94373d1a0e24bdedc6ffc92ff
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-10-28 18:17:54 +0530

    Delete week3 directory

--- week3/day2/IphoneBottomView.java
+++
@@ -1,9 +0,0 @@
-package week3.day2;
-
-public interface IphoneBottomView {
-
-	
-	public void chargingPort();
-	
-	
-}
--- week3/day2/IphoneRIghtSideView.java
+++
@@ -1,8 +0,0 @@
-package week3.day2;
-
-public interface IphoneRIghtSideView {
-
-	public void powerButton();
-	
-	
-}
